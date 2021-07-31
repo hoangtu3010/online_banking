50f9242830428903265d5789dbf20c8282062b20
hoang <hoangtu2378@gmail.com>
2021-07-31 19:21:29 +0700

    up-file-tu

---
+++ app/Http/Controllers/User/Auth/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\User\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request){
+        if ($request->method() == "GET"){
+            return view("login & register.login");
+        }
+
+        $credentials = $request->only(["email", "password"]);
+        if (Auth::guard("user")->attempt($credentials)){
+            return redirect()->to("user");
+        }
+
+        if (Auth::guard("admin")->attempt($credentials)){
+            return redirect()->to("admin");
+        }
+
+        return redirect()->back()->withInput();
+    }
+
+    public function register(Request $request){
+        return view("login & register.register");
+    }
+}
