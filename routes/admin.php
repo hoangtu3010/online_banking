547e47e86ed1b5974391445cc9c66799b112dfab
DELL <buichithanh24122000@gmail.com>
2021-08-02 13:23:23 +0700

    up

--- routes/admin.php
+++ routes/admin.php
@@ -1,9 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
 Route::middleware("auth:admin")->group(function (){
     Route::get('/', function () {
-        return view('home');
+        echo "admin";
+        return view('Admin.components.admin-home');
     });
+    Route::get('/create',[AdminController::class,"AdminCreate"]);
+    Route::get('/mod',[AdminController::class,"AdminMod"]);
+    Route::get('/setting/{id}',[AdminController::class,"AdminSetting"]);
+    Route::get('/customer',[AdminController::class,"AdminCustomer"]);
 });
