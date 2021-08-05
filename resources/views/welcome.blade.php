b20ca896186abdabcce11932bdca3654291341c2
thieugia882014 <70931357+thieugia882014@users.noreply.github.com>
2021-08-05 17:51:51 +0700

    ...

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,12 @@
 <section>
     <header>
         <div class="container">
-            <div class="row">
-                <div class="col-md-2">
-                    <img src="./public/imgs/logo.png" alt="#">
+            <div class="row mb-5">
+                <div class="col-md-2 scrolling ">
+                    <img src="{{ url('imgs/logo.png') }}" alt="">
                 </div>
-                <div class="col-md-6"><ul class="nav justify-content-end">
+                <div class="col-md-6">
+                    <ul class="nav justify-content-end">
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
@@ -51,85 +52,104 @@
         <div class="swiper-content">
             <div class="swiper-container">
                 <div class="swiper-wrapper">
-                    <div class="swiper-slide">Slide 1</div>
-                    <div class="swiper-slide">Slide 2</div>
-                    <div class="swiper-slide">Slide 3</div>
-                    <div class="swiper-slide">Slide 4</div>
-                    <div class="swiper-slide">Slide 5</div>
-                    <div class="swiper-slide">Slide 6</div>
-                    <div class="swiper-slide">Slide 7</div>
-                    <div class="swiper-slide">Slide 8</div>
-                    <div class="swiper-slide">Slide 9</div>
-                    <div class="swiper-slide">Slide 10</div>
+                    <div class="swiper-slide">
+                           <div class="box-content">
+                               <h3>
+                                   Card Features and Benefits
+                                   More Features
+                               </h3>
+                               <p>Proin sed felis ultrices, tristique eros eu, accumsan justo.</p>
+                               <button class="btn btn-primary">More Features</button>
+
+                           </div>
+                            <img src="{{url("imgs/Untitled.png")}}" alt="">
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="box-content">
+                            <h3>
+                                Card Features and Benefits
+                                More Features
+                            </h3>
+                            <p>Proin sed felis ultrices, tristique eros eu, accumsan justo.</p>
+                            <button class="btn btn-primary">More Features</button>
+                        </div>
+                        <img class="img-visa" src="{{url("imgs/slider-img-3.jpg")}}" alt="">
+                        <img class="bank" src="{{url("imgs/phong.png")}}" alt="">
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="distance">
-                        <h2 class="text-center">Card Features and Benefits</h2>
-                        <p class="text-center mb-5"><a>Blog</a></p>
-                    </div>
-                    <div class="row">
-                    <div class="col-md-4">
-                        <div class="tuan">
-                            <img src="./public/imgs/logo.png" alt="#">
-                            <h3>surplus</h3>
-                            <p>chuyển khoản</p>
+
+        <div class="box-content2">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="distance">
+                            <h2 class="text-center">Card Features and Benefits</h2>
+                            <p class="text-center mb-5"><a>Blog</a></p>
                         </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="tuan">
-                            <img src="./public/imgs/logo.png" alt="#">
-                            <h3>surplus</h3>
-                            <p>chuyển khoản</p>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="tuan">
-                            <img src="./public/imgs/logo.png" alt="#">
-                            <h3>surplus</h3>
-                            <p>chuyển khoản</p>
-                        </div>
-                    </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-4">
-                            <div class="tuan">
-                                <img src="./public/imgs/logo.png" alt="#">
-                                <h3>surplus</h3>
-                                <p>chuyển khoản</p>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="tuan text-center">
+                                    <img src="{{url("imgs/006-growth (1).jpg")}}" alt="">
+                                    <h3>surplus</h3>
+                                    <p>chuyển khoản</p>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="tuan text-center">
+                                    <img src="{{url("imgs/007-profits.jpg")}}" alt="">
+                                    <h3>Change</h3>
+                                    <p>chuyển khoản</p>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="tuan text-center">
+                                    <img src="{{url("imgs/009-worldwide.jpg")}}" alt="">
+                                    <h3>Infomation</h3>
+                                    <p>chuyển khoản</p>
+                                </div>
                             </div>
                         </div>
-                        <div class="col-md-4">
-                            <div class="tuan">
-                                <img src="./public/imgs/logo.png" alt="#">
-                                <h3>surplus</h3>
-                                <p>chuyển khoản</p>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="tuan text-center">
+                                    <img src="{{url("imgs/010-cash.jpg")}}" alt="">
+                                    <h3>surplus</h3>
+                                    <p>chuyển khoản</p>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="tuan">
-                                <img src="./public/imgs/logo.png" alt="#">
-                                <h3>surplus</h3>
-                                <p>chuyển khoản</p>
+                            <div class="col-md-4">
+                                <div class="tuan text-center">
+                                    <img src="{{url("imgs/011-money-2.jpg")}}" alt="">
+                                    <h3>History</h3>
+                                    <p>chuyển khoản</p>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="tuan text-center">
+                                    <img src="{{url("imgs/027-business-7.jpg")}}" alt="">
+                                    <h3>surplus</h3>
+                                    <p>chuyển khoản</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-md-12">
-                    <h2 class="text-center">Contacts - About Us</h2>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="tuan">
-                                <img src="./public/imgs/logo.png" alt="#">
+                    <div class="col-md-12">
+                        <h2 class="text-center">Contacts - About Us</h2>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="tuan text-center>
+                                    <img src="{{url("imgs/logo.png")}}" alt="">
+                                </div>
                             </div>
                         </div>
-                </div>
+                    </div>
                 </div>
             </div>
         </div>
+
     </div>
 </section>
 </body>
