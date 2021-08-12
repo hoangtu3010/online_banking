e1ab9f6073ccb753d9f599a40c0141ce01a10f25
truongquangdang97 <truongquangdang97@gmail.com>
2021-08-12 12:35:38 +0700

    quang up date

---
+++ app/Http/Controllers/ResetPassWordController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class ResetPassWordController extends Controller
+{
+    public function quen_mat_khau(){
+        return view("resetPassword.reset");
+    }
+
+    public function sendToEmail(Request $request){
+        $email = $request->email;
+
+        $checkUser = User::where(
+           'email',$email
+        )->first();
+
+        if (!$checkUser){
+            return redirect()->back()->with('error','Email này chưa được đăng kí tài khoản');
+        }
+
+        $code = bcrypt(md5(time().$email));
+        $checkUser->time_code=Carbon::now();
+        $checkUser->save();
+
+        $url =route('resetPass',['code'=>$checkUser->code,'email'=>$email]);
+
+        $data=[
+            'route'=>$url
+        ];
+
+        Mail::send('resetPassword.linkReset',$data,function ($message) use($checkUser){
+            $message->to($checkUser->email,'okokokokok')->subject('thay đổi mật khẩu ');
+        });
+
+        return redirect()->back()->with('message','Đã gửi thông tin lên gmail thành công ');
+    }
+
+    public function resetPass(Request $request){
+        $code= $request->code;
+        $email=$request->email;
+
+        $checkUser=User::where([
+           'code'=>$code,
+           'email'=>$email
+        ])->first();
+
+        if (!$checkUser){
+            return redirect('/')->with('error','xin loi');
+        }
+        return view('resetPassWord.formReset');
+    }
+
+    public function savePass(Request $request){
+        $code =$request->code;
+        $email = $request->email;
+        $checkUser= User::where([
+            "code"=>$code,
+            "email"=>$email
+        ])->first();
+
+        $checkUser->password=bcrypt($request->password);
+        $checkUser->save();
+
+        return view('resetPassword.successPass');
+    }
+
+}
