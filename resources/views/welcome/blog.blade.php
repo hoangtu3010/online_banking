5648d7b083f1691b202963f79de7d1149094f7f5
thieugia882014 <70931357+thieugia882014@users.noreply.github.com>
2021-08-16 18:52:31 +0700

    duc up

--- resources/views/welcome/blog.blade.php
+++ resources/views/welcome/blog.blade.php
@@ -4,36 +4,47 @@
     <div class="container">
         <div class="blog-main">
             <div class="row ">
-                <div class="col-md-6 blog-item">
+                <div class="col-md-7 blog-item">
+                    <div class="post-header">
+                        <h1 class="post-title">
+                            <a href="">All Posts</a>
+                        </h1>
+                        <div class="post-meta">
+                            <span class="post-meta-item post-author">
+                                <a>Home</a>
+                            </span>
+                            <span class="post-meta-item">
+                                <a>All Posts</a>
+                            </span>
+                        </div>
+                    </div>
                     <div class="post-image">
-                        <img src="https://cellphones.com.vn/sforum/wp-content/uploads/2018/11/3-8.png" alt="img">
+                        <img src="http://creditcard.axiomthemes.com/wp-content/uploads/2017/06/post-5-1170x658.jpg" width="700px" height="400px" alt="img">
                         <div class="mask"></div>
-                        <a class="post-icons">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        </a>
                     </div>
                     <div class="post-header">
                         <h3 class="post-title">
-                            <a href="">djfkjf</a>
+                            <a href="">Credit not as great as you would like?</a>
                         </h3>
                         <div class="post-meta">
                                             <span class="post-meta-item post-author">
-                                                <a>dfdf</a>
+                                                <a>Funding Trends</a>
                                             </span>
                             <span class="post-meta-item post-date">
-                                                <a>ggfg</a>
+                                                <a> June 26, 2017</a>
                                             </span>
                             <span class="post-meta-item post-comments">
 
-                                                <a>0-Comments</a>
+                                                <a>3-Comments</a>
                                             </span>
                         </div>
                         <div class="post-content">
-                            <div class="post-content-inner">
+                            <div class="post-content-inne">
                                 <p class="text-content">
-                                    fdsfsdfsf
+                                    Nullam tincidunt elit dolor, quis venenatis magna convallis at.
+                                    Nam fringilla mauris ut leo imperdiet, ut pulvinar sem eleifend. Sed iaculis pharetra interdum.
+                                    In pellentesque tempus magna, et volutpat eros aliquet non. Duis condimentum ligula nec justo viverra ultricies.
+                                    Integer non ipsum ac orci pharetra hendrerit et ut nisl. Suspendisse eu volutpat arcu. Etiam consectetur varius nulla, eget imperdiet dui…
                                 </p>
                                 <p style="margin-top: 25px">
                                     <a class="more-click" href="#">
@@ -43,6 +54,20 @@
                             </div>
                         </div>
                     </div>
+                    </div>
+
+                <div class="col-md-5 box-right">
+                    <div class="box-content">
+                        <div class="box">
+                            <h4 class="text-box">
+                                Recent Posts
+                            </h4>
+                            <div class="box-text1">
+                            <img src="http://creditcard.axiomthemes.com/wp-content/uploads/2017/06/post-5-120x88.jpg">
+                                ghyhghg
+                        </div>
+                    </div>
+                </div>
                 </div>
             </div>
         </div>
