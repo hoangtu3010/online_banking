ea52586af422b247d44d62cce551ae08a723810b
hoang <hoangtu2378@gmail.com>
2021-08-24 02:16:18 +0700

    up-tu

--- resources/views/User/BankAccount/BankAccount.blade.php
+++ resources/views/User/BankAccount/BankAccount.blade.php
@@ -1,5 +1,26 @@
 @extends("layout")
 @section("main")
+    <link rel="stylesheet" href="{{asset("css/form-style.css")}}">
+
+    <div class="bgr-head-list"></div>
+
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6 content-header-left">
+                    <a href="{{url("/user")}}">
+                        <i class="button-back ion-ios-arrow-thin-left"></i>
+                    </a>
+                    <h1>Bank Account</h1>
+                </div>
+                <div class="col-sm-6 action-header">
+                    <a href="{{url("user/bankAccount/link")}}">
+                        <button class="btn" style="border-radius: 20px">Add Bank Account</button>
+                    </a>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
     @php($i = false)
     @foreach($bank as $item)
         @if($item->user_id === Auth::user()->id)
@@ -10,47 +31,59 @@
     @foreach($data as $d)
         @if(Auth::user()->id == $d->id)
             @if($i == true)
+                <section class="content content-main">
+                    <div class="container-fluid">
+                        <div class="card-body table-responsive table-style">
+                            <table class="table table-hover table-form text-center">
 
-                <div class="row">
-                    <div class="left">
-                        <a href="{{url("user/bankAccount/link")}}" class="btn btn-outline-primary"> Add Bank account</a>
-                    </div>
-                    <div>
-                        <table class="table">
-
-                            <thead>
-                            <tr>
-                                <th>Stk</th>
-                                <th>Balance</th>
-                                <th colspan="2">Action</th>
-                                <th></th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($d->bankAccount as $b)
+                                <thead>
                                 <tr>
-                                    <td>{{$b->stk}}</td>
-                                    <td>{{$b->balance}}</td>
-                                    <td><a href="{{url("user/bankAccount/info",["id"=>$b->id])}}"
-                                           class="btn btn-outline-success">info</a>
-                                        <a href="{{url("user/bankAccount/transfer",["id"=>$b->id])}}"
-                                           class="btn btn-outline-success">transfer</a></td>
-                                    <td><a href="{{url("user/bankAccount/history",["id"=>$b->id])}}"
-                                           class="btn btn-outline-success">history</a></td>
+                                    <th>Account Number</th>
+                                    <th>Balance</th>
+                                    <th width="5%" colspan="3">Action</th>
                                 </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                @foreach($d->bankAccount as $b)
+                                    <tr>
+                                        <td>{{$b->stk}}</td>
+                                        <td>{{$b->balance}}</td>
+                                        <td>
+                                            <a href="{{url("user/bankAccount/transfer",["id"=>$b->id])}}">
+                                                <i class="fas fa-exchange-alt transfer"></i>
+                                            </a>
+                                        </td>
+                                        <td>
+                                            <a href="{{url("user/bankAccount/history",["id"=>$b->id])}}">
+                                                <i class="fas fa-history history"></i>
+                                            </a>
+                                        </td>
+                                        <td aria-label="Detail">
+                                            <a href="{{url("user/bankAccount/info",["id"=>$b->id])}}">
+                                                <i class="fas fa-info-circle detail"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
-
-                </div>
+                </section>
             @else
-                <h1> Bạn chưa có liên kết? hãy ABC XYZ</h1>
-                <div class="container">
-                    <div class="row">
-                        <a href="{{url("user/bankAccount/link")}}" class="btn btn-success"> Add bank account </a>
+                <section class="content content-main">
+                    <div class="container-fluid">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Account Number Null</h3>
+                            </div>
+                            <div class="card-body text-center">
+                                <p class="text-danger" style="font-size: 6rem"><i class="ion-close-circled"></i></p>
+                                <h3>You are not currently associated with any account, <br> please link to trade !</h3>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                </section>
             @endif
         @endif
     @endforeach
--- resources/views/User/BankAccount/link.blade.php
+++ resources/views/User/BankAccount/link.blade.php
@@ -1,23 +1,62 @@
 @extends("layout")
 @section("main")
-    <div class="row">
-        <div>
-            <form action="{{url("user/bankAccount/link")}}" method="post">
-                @csrf
-                <div class="form-group">
-                    <label for="">Stk</label>
-                    <input type="text" name="stk" placeholder="Bank account..." class="form-control">
+    <link rel="stylesheet" href="{{asset("css/form-style.css")}}">
+
+    <div class="bgr-head-list"></div>
+
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6 content-header-left">
+                    <a href="{{url("user/bankAccount")}}">
+                        <i class="button-back ion-ios-arrow-thin-left"></i>
+                    </a>
+                    <h1>Bank Account</h1>
                 </div>
-                <div class="form-group">
-                    <label for="">Pass</label>
-                    <input type="password" name="password" placeholder="Password..." class="form-control">
+                <div class="col-md-6">
+
                 </div>
-                <button class="btn btn-outline-success" type="submit">Submit</button>
-                @if($errors->any())
-                    <h4 class="text-red">{{$errors->first()}}</h4>
-                @endif
-                <a class="btn btn-outline-danger" href="{{url("user/bankAccount")}}">Back</a>
-            </form>
+            </div>
         </div>
-    </div>
+    </section>
+    <section class="content content-main">
+        <div class="container-fluid">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Add</h3>
+                </div>
+                <div class="card-body">
+                    <form class="row needs-validation" action="{{url("user/bankAccount/link")}}" method="post"
+                          novalidate>
+                        @csrf
+                        <div class="col-md-12 flex-column pl-3">
+                            <div class="form-group">
+                                <input name="stk" type="text" class="form-control" id="validationCustom01" placeholder=" " autocomplete="off"
+                                       required/>
+                                <label for="validationCustom01" class="form-label">Account number</label>
+                                <div class="invalid-feedback">
+                                    Please enter account account number.
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <input name="password" type="password" class="form-control" id="validationCustom02" placeholder=" "
+                                       required/>
+                                <label for="validationCustom02" class="form-label">Password</label>
+                                <div class="invalid-feedback">
+                                    Please enter password.
+                                </div>
+                            </div>
+                            @if($errors->any())
+                                <p class="text-red">{{$errors->first()}}</p>
+                            @endif
+                        </div>
+                        <div class="button-form">
+                            <button class="btn" type="submit" style="float: right">Add</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
