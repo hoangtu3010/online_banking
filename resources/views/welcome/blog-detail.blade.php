52d4e5f837a4a8c9295dd4a6a816ea0d80fe96c9
thieugia882014 <70931357+thieugia882014@users.noreply.github.com>
2021-08-23 19:44:55 +0700

    duc up

--- resources/views/welcome/blog-detail.blade.php
+++ resources/views/welcome/blog-detail.blade.php
@@ -1,6 +1,187 @@
 @extends("welcome.layout-welcome")
 @section("main")
+    <link rel="stylesheet" href="{{asset("css/blogDetail.css")}}">
     <div class="container">
-        <h1>Day la details</h1>
+        <aside1 class="blog-detail">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="info-detail">Credit not as great as you would like?</h2>
+                    <div class="breadcrumbs">
+                        <a href="breadcrumbs_item home">Home</a>
+                        <span class="breadcrumbs_delimiter"><i class="fas fa-chevron-right"></i></span>
+
+                        <a href="breadcrumbs_item cat_post ">All Posts</a>
+                        <span class="breadcrumbs_delimiter"><i class="fas fa-chevron-right"></i></span>
+
+                        <a href="breadcrumbs_item cat_post ">Funding Trends</a>
+                        <span class="breadcrumbs_delimiter"><i class="fas fa-chevron-right"></i></span>
+
+                        <a href="breadcrumbs_item current">Credit not as great as you would like?</a>
+                    </div>
+                </div>
+
+            </div>
+        </aside1>
+
+        <div class="blog-main">
+            <div class="row">
+                <div class="col-md-7 blog-item">
+
+                    <div class="post-image">
+                        <img src="http://creditcard.axiomthemes.com/wp-content/uploads/2017/06/post-5-1170x658.jpg"
+                             width="700px" height="400px" alt="img">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="post-header">
+                        <h3 class="post-title">
+                            <a href="">Credit not as great as you would like?</a>
+                        </h3>
+                        <div class="post-meta">
+                                            <span class="post-meta-item post-author">
+                                                <a>Funding Trends</a>
+                                            </span>
+                            <span class="post-meta-item post-date">
+                                                <a> June 26, 2017</a>
+                                            </span>
+                            <span class="post-meta-item post-comments">
+
+                                                <a>3-Comments</a>
+                                            </span>
+                        </div>
+                        <div class="post-content">
+                            <div class="post-content-inne">
+                                <p class="text-content">
+                                    Nullam tincidunt elit dolor, quis venenatis magna convallis at.
+                                    Nam fringilla mauris ut leo imperdiet, ut pulvinar sem eleifend. Sed iaculis
+                                    pharetra interdum.
+                                    In pellentesque tempus magna, et volutpat eros aliquet non. Duis condimentum ligula
+                                    nec justo viverra ultricies.
+                                    Integer non ipsum ac orci pharetra hendrerit et ut nisl. Suspendisse eu volutpat
+                                    arcu. Etiam consectetur varius nulla, eget imperdiet dui…
+                                </p>
+                                <p style="margin-top: 25px">
+                                    <a class="more-click" href="#">
+                                        Read More
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-5 box-right">
+                    <div class="box-content">
+                        <div class="box">
+                            <h4 class="text-box">
+                                Recent Posts
+                            </h4>
+                            <div class="box-text1">
+                                <img
+                                    src="http://creditcard.axiomthemes.com/wp-content/uploads/2017/06/post-5-120x88.jpg">
+                                <p class="box-blog">Credit not as great as you would like?
+                                    <br>
+                                    <span class="box-span">June 26, 2017</span>
+                                </p>
+                            </div>
+
+                            <div class="box-text1">
+                                <img
+                                    src="http://creditcard.axiomthemes.com/wp-content/uploads/2017/06/post-5-120x88.jpg">
+                                <p class="box-blog">Credit not as great as you would like?
+                                    <br>
+                                    <span class="box-span">June 26, 2017</span>
+                                </p>
+                            </div>
+
+                            <div class="box-text1">
+                                <img
+                                    src="http://creditcard.axiomthemes.com/wp-content/uploads/2017/06/post-5-120x88.jpg">
+                                <p class="box-blog">Credit not as great as you would like?
+                                    <br>
+                                    <span class="box-span">June 26, 2017</span>
+                                </p>
+                            </div>
+
+                            <div class="box-text1">
+                                <img
+                                    src="http://creditcard.axiomthemes.com/wp-content/uploads/2017/06/post-5-120x88.jpg">
+                                <p class="box-blog">Credit not as great as you would like?
+                                    <br>
+                                    <span class="box-span">June 26, 2017</span>
+                                </p>
+                            </div>
+
+                            <div class="box-text1">
+                                <img
+                                    src="http://creditcard.axiomthemes.com/wp-content/uploads/2017/06/post-5-120x88.jpg">
+                                <p class="box-blog">Credit not as great as you would like?
+                                    <br>
+                                    <span class="box-span">June 26, 2017</span>
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-7 blog-item">
+                    <div class="post-image">
+                        <img src="http://creditcard.axiomthemes.com/wp-content/uploads/2017/06/post-5-1170x658.jpg"
+                             width="700px" height="400px" alt="img">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="post-header">
+                        <h3 class="post-title">
+                            <a href="">Credit not as great as you would like?</a>
+                        </h3>
+                        <div class="post-meta">
+                                            <span class="post-meta-item post-author">
+                                                <a>Funding Trends</a>
+                                            </span>
+                            <span class="post-meta-item post-date">
+                                                <a> June 26, 2017</a>
+                                            </span>
+                            <span class="post-meta-item post-comments">
+
+                                                <a>3-Comments</a>
+                                            </span>
+                        </div>
+                        <div class="post-content">
+                            <div class="post-content-inne">
+                                <p class="text-content">
+                                    Nullam tincidunt elit dolor, quis venenatis magna convallis at.
+                                    Nam fringilla mauris ut leo imperdiet, ut pulvinar sem eleifend. Sed iaculis
+                                    pharetra interdum.
+                                    In pellentesque tempus magna, et volutpat eros aliquet non. Duis condimentum ligula
+                                    nec justo viverra ultricies.
+                                    Integer non ipsum ac orci pharetra hendrerit et ut nisl. Suspendisse eu volutpat
+                                    arcu. Etiam consectetur varius nulla, eget imperdiet dui…
+                                </p>
+                                <p style="margin-top: 25px">
+                                    <a class="more-click" href="#">
+                                        Read More
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-5 box-right">
+                    <div class="box-content-calender">
+                        <div class="box-calender">
+                            <h5 class="text-box">
+                                Calender
+                            </h5>
+                            <aside class="widget widget_calendar">
+
+                            </aside>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 @endsection
--- resources/views/welcome/blog.blade.php
+++ resources/views/welcome/blog.blade.php
@@ -20,7 +20,8 @@
                         </div>
                     </div>
                     <div class="post-image">
-                        <img src="http://creditcard.axiomthemes.com/wp-content/uploads/2017/06/post-5-1170x658.jpg" width="700px" height="400px" alt="img">
+                        <img src="http://creditcard.axiomthemes.com/wp-content/uploads/2017/06/post-5-1170x658.jpg"
+                             width="700px" height="400px" alt="img">
                         <div class="mask"></div>
                     </div>
                     <div class="post-header">
@@ -43,9 +44,12 @@
                             <div class="post-content-inne">
                                 <p class="text-content">
                                     Nullam tincidunt elit dolor, quis venenatis magna convallis at.
-                                    Nam fringilla mauris ut leo imperdiet, ut pulvinar sem eleifend. Sed iaculis pharetra interdum.
-                                    In pellentesque tempus magna, et volutpat eros aliquet non. Duis condimentum ligula nec justo viverra ultricies.
-                                    Integer non ipsum ac orci pharetra hendrerit et ut nisl. Suspendisse eu volutpat arcu. Etiam consectetur varius nulla, eget imperdiet dui…
+                                    Nam fringilla mauris ut leo imperdiet, ut pulvinar sem eleifend. Sed iaculis
+                                    pharetra interdum.
+                                    In pellentesque tempus magna, et volutpat eros aliquet non. Duis condimentum ligula
+                                    nec justo viverra ultricies.
+                                    Integer non ipsum ac orci pharetra hendrerit et ut nisl. Suspendisse eu volutpat
+                                    arcu. Etiam consectetur varius nulla, eget imperdiet dui…
                                 </p>
                                 <p style="margin-top: 25px">
                                     <a class="more-click" href="#">
@@ -55,7 +59,7 @@
                             </div>
                         </div>
                     </div>
-                    </div>
+                </div>
 
                 <div class="col-md-5 box-right">
                     <div class="box-content">
@@ -64,11 +68,110 @@
                                 Recent Posts
                             </h4>
                             <div class="box-text1">
-                            <img src="http://creditcard.axiomthemes.com/wp-content/uploads/2017/06/post-5-120x88.jpg">
-                                ghyhghg
+                                <img
+                                    src="http://creditcard.axiomthemes.com/wp-content/uploads/2017/06/post-5-120x88.jpg">
+                                <p class="box-blog">Credit not as great as you would like?
+                                    <br>
+                                    <span class="box-span">June 26, 2017</span>
+                                </p>
+                            </div>
+
+                            <div class="box-text1">
+                                <img
+                                    src="http://creditcard.axiomthemes.com/wp-content/uploads/2017/06/post-5-120x88.jpg">
+                                <p class="box-blog">Credit not as great as you would like?
+                                    <br>
+                                    <span class="box-span">June 26, 2017</span>
+                                </p>
+                            </div>
+
+                            <div class="box-text1">
+                                <img
+                                    src="http://creditcard.axiomthemes.com/wp-content/uploads/2017/06/post-5-120x88.jpg">
+                                <p class="box-blog">Credit not as great as you would like?
+                                    <br>
+                                    <span class="box-span">June 26, 2017</span>
+                                </p>
+                            </div>
+
+                            <div class="box-text1">
+                                <img
+                                    src="http://creditcard.axiomthemes.com/wp-content/uploads/2017/06/post-5-120x88.jpg">
+                                <p class="box-blog">Credit not as great as you would like?
+                                    <br>
+                                    <span class="box-span">June 26, 2017</span>
+                                </p>
+                            </div>
+
+                            <div class="box-text1">
+                                <img
+                                    src="http://creditcard.axiomthemes.com/wp-content/uploads/2017/06/post-5-120x88.jpg">
+                                <p class="box-blog">Credit not as great as you would like?
+                                    <br>
+                                    <span class="box-span">June 26, 2017</span>
+                                </p>
+                            </div>
+
                         </div>
                     </div>
                 </div>
+
+                <div class="col-md-7 blog-item">
+                    <div class="post-image">
+                        <img src="http://creditcard.axiomthemes.com/wp-content/uploads/2017/06/post-5-1170x658.jpg"
+                             width="700px" height="400px" alt="img">
+                        <div class="mask"></div>
+                    </div>
+                    <div class="post-header">
+                        <h3 class="post-title">
+                            <a href="">Credit not as great as you would like?</a>
+                        </h3>
+                        <div class="post-meta">
+                                            <span class="post-meta-item post-author">
+                                                <a>Funding Trends</a>
+                                            </span>
+                            <span class="post-meta-item post-date">
+                                                <a> June 26, 2017</a>
+                                            </span>
+                            <span class="post-meta-item post-comments">
+
+                                                <a>3-Comments</a>
+                                            </span>
+                        </div>
+                        <div class="post-content">
+                            <div class="post-content-inne">
+                                <p class="text-content">
+                                    Nullam tincidunt elit dolor, quis venenatis magna convallis at.
+                                    Nam fringilla mauris ut leo imperdiet, ut pulvinar sem eleifend. Sed iaculis
+                                    pharetra interdum.
+                                    In pellentesque tempus magna, et volutpat eros aliquet non. Duis condimentum ligula
+                                    nec justo viverra ultricies.
+                                    Integer non ipsum ac orci pharetra hendrerit et ut nisl. Suspendisse eu volutpat
+                                    arcu. Etiam consectetur varius nulla, eget imperdiet dui…
+                                </p>
+                                <p style="margin-top: 25px">
+                                    <a class="more-click" href="#">
+                                        Read More
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-5 box-right">
+                    <div class="box-content-calender">
+                        <div class="box-calender">
+                            <h5 class="text-box">
+                                Calender
+                            </h5>
+                            <aside class="widget widget_calendar">
+
+                            </aside>
+
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
