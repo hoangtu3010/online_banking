d03b58305337eaca24fc93d478893c7baf34a69b
hoang <hoangtu2378@gmail.com>
2021-08-31 22:51:37 +0700

    up-tu

--- app/Models/SaveMoney.php
+++ app/Models/SaveMoney.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class SaveMoney extends Model
 {
     use HasFactory;
-    protected $table ='SaveMoney';
+    protected $table ='savemoney';
     protected $fillable =[
         'stk',
         'money',
